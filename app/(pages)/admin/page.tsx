0d69a03ee64f645711ed2981f42d7b2d48f06858
alireza <alirezatt705@gmail.com>
2025-08-15 23:51:07 +0430

    back to prev version

--- app/(pages)/admin/page.tsx
+++ app/(pages)/admin/page.tsx
@@ -1,29 +1,13 @@
-// // import AdminDashboard from "@/app/components/admin/home";
-// // import AdminDashboardSkeleton from "@/app/components/admin/home/skeleton";
-// import React, { Suspense } from "react";
-// export const dynamic = "force-dynamic";
-// export const revalidate = 0;
+import AdminDashboard from "@/app/components/admin/home";
+import AdminDashboardSkeleton from "@/app/components/admin/home/skeleton";
+import React, { Suspense } from "react";
 
-// const AdminDashboardPage = () => {
-//   return (
-//     <div>admin main page</div>
-//     // <Suspense fallback={<AdminDashboardSkeleton />}>
-//     //   <AdminDashboard />
-//     // </Suspense>
-//   );
-// };
+const AdminDashboardPage = () => {
+  return (
+    <Suspense fallback={<AdminDashboardSkeleton />}>
+      <AdminDashboard />
+    </Suspense>
+  );
+};
 
-// export default AdminDashboardPage;
-
-
-
-// app/admin/page.tsx   new code
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-import React from "react";
-
-export default function AdminDashboardPage() {
-  return <div>admin main page</div>;
-}
-
+export default AdminDashboardPage;
