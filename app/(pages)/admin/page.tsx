051168ef2f8f3bb95b2523456e5f6e7675e5274e
alireza <alirezatt705@gmail.com>
2025-08-15 23:38:28 +0430

    fix error

--- app/(pages)/admin/page.tsx
+++ app/(pages)/admin/page.tsx
@@ -1,6 +1,8 @@
-import AdminDashboard from "@/app/components/admin/home";
-import AdminDashboardSkeleton from "@/app/components/admin/home/skeleton";
+// import AdminDashboard from "@/app/components/admin/home";
+// import AdminDashboardSkeleton from "@/app/components/admin/home/skeleton";
 import React, { Suspense } from "react";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 const AdminDashboardPage = () => {
   return (
