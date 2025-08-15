0fe3013fce9a848ba69e3fec068eb59e3f324e45
alireza <alirezatt705@gmail.com>
2025-08-15 23:49:41 +0430

    test deploy

--- app/components/admin/home/index.tsx
+++ app/components/admin/home/index.tsx
@@ -1,35 +1,34 @@
 // app/admin/page.tsx or app/admin/dashboard/page.tsx
-// import { fetchProjects } from "@/app/lib/fetch/fetchProjects";
-// import { fetchSkills } from "@/app/lib/fetch/fetchSkills";
-// import { fetchExperiences } from "@/app/lib/fetch/fetchExperiences";
-// import { fetchContactInfo } from "@/app/lib/fetch/admin/fetchContactInfo";
-// import { fetchSubmissions } from "@/app/lib/fetch/admin/fetchSubmissions";
-// import { fetchAboutData } from "@/app/lib/fetch/fetchAbout";
-// import AdminDashboardContent from "./content";
+import { fetchProjects } from "@/app/lib/fetch/fetchProjects";
+import { fetchSkills } from "@/app/lib/fetch/fetchSkills";
+import { fetchExperiences } from "@/app/lib/fetch/fetchExperiences";
+import { fetchContactInfo } from "@/app/lib/fetch/admin/fetchContactInfo";
+import { fetchSubmissions } from "@/app/lib/fetch/admin/fetchSubmissions";
+import { fetchAboutData } from "@/app/lib/fetch/fetchAbout";
+import AdminDashboardContent from "./content";
 
 
 
-export default function AdminDashboard() {
-  // const [projects, skills, experienceData, contactInfo, submissions, aboutData] =
-  //   await Promise.all([
-  //     fetchProjects(),
-  //     fetchSkills(),
-  //     fetchExperiences(),
-  //     fetchContactInfo(),
-  //     fetchSubmissions(),
-  //     fetchAboutData(),
-  //   ]);
+export default async function AdminDashboard() {
+  const [projects, skills, experienceData, contactInfo, submissions, aboutData] =
+    await Promise.all([
+      fetchProjects(),
+      fetchSkills(),
+      fetchExperiences(),
+      fetchContactInfo(),
+      fetchSubmissions(),
+      fetchAboutData(),
+    ]);
 
   return (
-    <div>admin sub page</div>
-    // <AdminDashboardContent
-    //   projects={projects}
-    //   skills={skills}
-    //   experienceData={experienceData}
-    //   contactInfo={contactInfo}
-    //   submissions={submissions}
-    //   aboutData={aboutData}
-    // />
+    <AdminDashboardContent
+      projects={projects}
+      skills={skills}
+      experienceData={experienceData}
+      contactInfo={contactInfo}
+      submissions={submissions}
+      aboutData={aboutData}
+    />
   );
 }
 
