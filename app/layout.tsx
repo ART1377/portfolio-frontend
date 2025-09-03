b4e455d65e8796498518c403701b2789ce312adb
Alireza <alirezatt705@gmail.com>
2025-09-04 03:15:41 +0330

    replaced my real name

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,21 @@ import "./styles/globals.css";
 import MainProvider from "./providers/main-provider/mainProvider";
 
 export const metadata: Metadata = {
-  title: "Alex Johnson - Frontend Developer | Portfolio",
+  title: "Alireza Tahavori - Frontend Developer | Portfolio",
   description:
-    "Explore Alex Johnson’s portfolio showcasing frontend development projects, React apps, and UI/UX design work.",
+    "Explore Alireza Tahavori’s portfolio showcasing frontend development projects, React apps, and UI/UX design work.",
   openGraph: {
-    title: "Alex Johnson - Frontend Developer Portfolio",
+    title: "Alireza Tahavori - Frontend Developer Portfolio",
     description:
       "Explore React projects, frontend apps, and UI/UX case studies.",
     url: "https://portfolio-frontend-vert-gamma.vercel.app/",
-    siteName: "Alex Johnson Portfolio",
+    siteName: "Alireza Tahavori Portfolio",
     images: [
       {
         url: "https://portfolio-frontend-vert-gamma.vercel.app/preview.png",
         width: 1200,
         height: 630,
-        alt: "Alex Johnson Portfolio Preview",
+        alt: "Alireza Tahavori Portfolio Preview",
       },
     ],
     locale: "en_US",
@@ -26,13 +26,12 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Alex Johnson - Frontend Developer Portfolio",
+    title: "Alireza Tahavori - Frontend Developer Portfolio",
     description:
       "Frontend development portfolio showcasing React, Next.js, and UI design work.",
     images: ["https://portfolio-frontend-vert-gamma.vercel.app/preview.png"],
   },
 };
-
 
 export default async function RootLayout({
   children,
